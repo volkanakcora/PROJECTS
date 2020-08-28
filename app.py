23ad853d75ecfcca0fa45686e7456ea96cb3d6d6
volkanakcora <44731403+volkanakcora@users.noreply.github.com>
2020-08-28 13:43:23 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd 
+import numpy as np 
+import flask
+import pickle
+from flask import Flask, render_template, request
+from sklearn import preprocessing
+from sklearn.neighbors import KNeighborsClassifier 
+import os 
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+
+
+app=Flask(__name__)
+
+@app.route("/")
+
+def index():
+ return flask.render_template('index.html')
+
+
+def ValuePredictor(to_predict_list):
+ to_predict = np.array(to_predict_list).reshape(1,4)
+ loaded_model = pickle.load(open("model.pkl","rb"))
+ result = loaded_model.predict(to_predict)
+ return result[0]
+
+
+@app.route("/predict",methods = ["POST"])
+
+
+def result():
+ if request.method == "POST":
+    to_predict_list = request.form.to_dict()
+    to_predict_list=list(to_predict_list.values())
+    to_predict_list = list(map(float, to_predict_list))
+    result = ValuePredictor(to_predict_list)
+    prediction = str(result)
+
+ return render_template("predict.html",prediction=prediction)
+
+
+if __name__ == "__main__":
+ app.run(debug=True)
